9cd997b0d3ac837f239d335c3243212bdeccf654
TateB <tate@ens.domains>
2022-01-18 17:27:58 +1100

    added message in nextjs config

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ module.exports = {
   reactStrictMode: true,
   experimental: {
     styledComponents: true,
+    // remove this once this PR is merged to main: https://github.com/vercel/next.js/pull/33236
     swcFileReading: false,
   },
   swcMinify: true,
