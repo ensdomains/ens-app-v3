7b10117c56eed746775aa5722fcbe8fc68d289b9
TateB <yo@taytems.xyz>
2022-05-12 16:15:00 +1000

    use different zorb gradient

--- src/utils/gradient.ts
+++ src/utils/gradient.ts
@@ -164,33 +164,23 @@ export const zorbImageSVG = (input: string, type: 'name' | 'address') => {
   const bytes = type === 'name' ? namehash(input) : input
   const gradientInfo = gradientForBytes(bytes)
   return `
-    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 110 110">
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 110 110">
     <defs>
-      <radialGradient
-        id="gzr"
-        gradientTransform="translate(66.4578 24.3575) scale(75.2908)"
-        gradientUnits="userSpaceOnUse"
-        r="1"
-        cx="0"
-        cy="0%"
-        >
-        <stop offset="15.62%" stop-color="${gradientInfo[0]}" />
-        <stop offset="39.58%" stop-color="${gradientInfo[1]}" />
-        <stop offset="72.92%" stop-color="${gradientInfo[2]}" />
-        <stop offset="90.63%" stop-color="${gradientInfo[3]}" />
-        <stop offset="100%" stop-color="${gradientInfo[4]}" />
-      </radialGradient>
+      <linearGradient id="gzr" x1="106.975" y1="136.156" x2="-12.9815" y2="13.5347" gradientUnits="userSpaceOnUse">
+        gradientTransform="translate(131.638 129.835) rotate(-141.194) scale(185.582)">
+        <stop offset="0.1562" stop-color="${gradientInfo[0]}" />
+        <stop offset="0.3958" stop-color="${gradientInfo[1]}" />
+        <stop offset="0.7292" stop-color="${gradientInfo[2]}" />
+        <stop offset="0.9063" stop-color="${gradientInfo[3]}" />
+        <stop offset="1" stop-color="${gradientInfo[4]}" />
+      </linearGradient>
     </defs>
     <g transform="translate(5,5)">
       <path
         d="M100 50C100 22.3858 77.6142 0 50 0C22.3858 0 0 22.3858 0 50C0 77.6142 22.3858 100 50 100C77.6142 100 100 77.6142 100 50Z"
-        fill="url(#gzr)"
-      /><path
-        stroke="rgba(0,0,0,0.075)"
-        fill="transparent"
-        stroke-width="1"
-        d="M50,0.5c27.3,0,49.5,22.2,49.5,49.5S77.3,99.5,50,99.5S0.5,77.3,0.5,50S22.7,0.5,50,0.5z"
-      />
+        fill="url(#gzr)" />
+      <path stroke="rgba(0,0,0,0.075)" fill="transparent" stroke-width="1"
+        d="M50,0.5c27.3,0,49.5,22.2,49.5,49.5S77.3,99.5,50,99.5S0.5,77.3,0.5,50S22.7,0.5,50,0.5z" />
     </g>
   </svg>
     `
