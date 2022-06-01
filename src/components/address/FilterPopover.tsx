2d3327fdf4126fd118cb0cc3c011b4cecac4cf0c
storywithoutend <storywithoutend@gmail.com>
2022-06-01 16:48:07 +0800

    update filter

--- src/components/address/FilterPopover.tsx
+++ src/components/address/FilterPopover.tsx
@@ -1,38 +1,51 @@
 import { Checkbox, Field } from '@ensdomains/thorin'
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
 import SortControl, {
   SortType,
   SortDirection,
 } from '../@molecules/SortControl/SortControl'
 
-const PopoverContainer = styled.div`
-  ${({ theme }) => `
+const PopoverContainer = styled.div(
+  ({ theme }) => css`
     display: flex;
     flex-direction: column;
     gap: ${theme.space['3']};
     padding: ${theme.space['3']};
     width: 90vw;
-    max-width: 310px;    
+    max-width: 310px;
     background: ${theme.colors.white};
     border: 1px solid ${theme.colors.borderTertiary};
     box-shadow: ${theme.boxShadows['0.02']};
-  `}
-`
+    border-radius: ${theme.space['4']};
+  `,
+)
 
 const CheckBoxesWrapper = styled.div`
   display: flex;
+  justify-content: flex-start;
   flex-wrap: wrap;
   gap: 10px;
 `
 
 const CheckBoxWrapper = styled.div`
   ${({ theme }) => `
-    padding: ${theme.space['2']};
+    padding: ${theme.space['1']} ${theme.space['2']};
     background: ${theme.colors.backgroundSecondary};
     border: 1px solid ${theme.colors.borderSecondary};
     border-radius: ${theme.space['2.5']};
   `}
 `
+
+const CheckBoxLabel = styled.span(
+  ({ theme }) => css`
+    font-family: ${theme.fonts.sans};
+    font-weight: ${theme.fontWeights.bold};
+    font-size: ${theme.fontSizes.root};
+    line-height: ${theme.lineHeights['1.375']};
+    letter-spacing: ${theme.letterSpacings['-0.01']};
+    color: ${theme.colors.black};
+  `,
+)
 
 type PopoverProps = {
   sortBy: SortType
@@ -44,6 +57,7 @@ const FilterPopover = ({ sortBy, sortDirection }: PopoverProps) => {
     type: sortBy,
     direction: sortDirection,
   }
+
   return (
     <PopoverContainer>
       <SortControl value={initialSortValue} />
@@ -51,7 +65,7 @@ const FilterPopover = ({ sortBy, sortDirection }: PopoverProps) => {
         <CheckBoxesWrapper>
           {['registrant', 'controller', 'owner'].map((type) => (
             <CheckBoxWrapper>
-              <Checkbox label={type} />
+              <Checkbox label={<CheckBoxLabel>{type}</CheckBoxLabel>} />
             </CheckBoxWrapper>
           ))}
         </CheckBoxesWrapper>
