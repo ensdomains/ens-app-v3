6a6f57dec093b7c72adedac94c43450513530b05
storywithoutend <storywithoutend@gmail.com>
2022-06-09 14:09:19 +0800

    test with no fallback in getStaticPaths

--- src/pages/address/[address].tsx
+++ src/pages/address/[address].tsx
@@ -244,7 +244,7 @@ export async function getStaticProps() {
 export const getStaticPaths: GetStaticPaths<{ slug: string }> = async () => {
   return {
     paths: [],
-    fallback: 'blocking',
+    fallback: false,
   }
 }
 
