769cfe6fe2733f01e45cd6b4ae499c6593deb678
Leon Talbert <leon.talbert@gmail.com>
2022-06-09 10:50:55 +0800

    registration date

--- src/components/pages/profile/[name]/details/More/More.tsx
+++ src/components/pages/profile/[name]/details/More/More.tsx
@@ -1,8 +1,8 @@
 import { useEffect, useState } from 'react'
 import styled, { css } from 'styled-components'
 import { utils, BigNumber } from 'ethers'
-import { useProvider } from 'wagmi'
-import { Typography } from '@ensdomains/thorin'
+import { useProvider, useNetwork } from 'wagmi'
+import { Typography, Button } from '@ensdomains/thorin'
 import { useRouter } from 'next/router'
 
 import mq from '@app/mediaQuery'
@@ -167,19 +167,47 @@ const TokenId = () => {
 
   return (
     <div>
-      <RecordItem itemKey="labelhash" value={labelHash} hasBackground={false} />
-      <RecordItem itemKey="Token Id" value={tokenId} hasBackground={false} />
+      <RecordItem itemKey="hex" value={labelHash} hasBackground={false} />
+      <RecordItem itemKey="decimal" value={tokenId} hasBackground={false} />
     </div>
   )
 }
 
-// const createEtherscanLink = (network, transaction) => {}
+function getEtherScanLink(networkId) {
+  switch (networkId) {
+    case 1:
+    case '1':
+      return 'https://etherscan.io/'
+    case 3:
+    case '3':
+      return 'https://ropsten.etherscan.io/'
+    case 4:
+    case '4':
+      return 'https://rinkeby.etherscan.io/'
+    case 5:
+    case '5':
+      return 'https://goerli.etherscan.io/'
+    default:
+      return 'https://etherscan.io/'
+  }
+}
+
+const RegistrationDateContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`
 
 const RegistrationDate = () => {
   const router = useRouter()
   const { name } = router.query
-  const { history = {}, loading } = useGetHistory(name)
+  const { history = {}, isLoading } = useGetHistory(name)
   const provider = useProvider()
+  const {
+    activeChain: { id },
+  } = useNetwork()
+
+  console.log('networkId: ', id)
 
   const [registrationData, setRegistrationData] = useState({
     registrationDate: null,
@@ -187,10 +215,12 @@ const RegistrationDate = () => {
   })
 
   useEffect(() => {
-    console.log('loading: ', loading)
+    console.log('isLoading: ', isLoading)
     const getReigstartionData = async () => {
-      if (!loading) {
+      if (!isLoading) {
         const registration = history?.registration?.[0]
+
+        console.log('registration: ', registration)
 
         if (registration) {
           const registrationBlock = registration?.blockNumber
@@ -205,15 +235,34 @@ const RegistrationDate = () => {
 
           setRegistrationData({
             registrationDate: date.toString(),
-            transactionHash: block.hash,
+            transactionHash: registration.transactionHash,
           })
         }
       }
     }
     getReigstartionData()
-  }, [name, provider, loading])
+  }, [name, provider, isLoading])
 
-  return <a href="">{registrationData.registrationDate}</a>
+  console.log('registrationData: ', registrationData)
+  console.log('link: ', `${getEtherScanLink(id)}`)
+
+  return (
+    <RegistrationDateContainer>
+      <Typography>{registrationData.registrationDate}</Typography>
+      <div style={{ maxWidth: 300 }}>
+        <Button
+          as="a"
+          href={`${getEtherScanLink(id)}/tx/${
+            registrationData.transactionHash
+          }`}
+          target="_blank"
+          size="small"
+        >
+          View on etherscan
+        </Button>
+      </div>
+    </RegistrationDateContainer>
+  )
 }
 
 const data: AccordionData = [
