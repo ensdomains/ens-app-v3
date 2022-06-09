7e67016a0fdbcfdb39cae241647b1947ba18e6fa
Leon Talbert <leon.talbert@gmail.com>
2022-06-09 20:18:33 +0800

    accordion tests

--- src/components/pages/profile/[name]/details/More/Accordion.test.tsx
+++ src/components/pages/profile/[name]/details/More/Accordion.test.tsx
@@ -1,4 +1,30 @@
+import { render, screen, fireEvent } from '@app/test-utils'
+
+import Accordion, { AccordionData } from './Accordion'
+
+const data: AccordionData = [
+  {
+    title: 'Item 1',
+    body: 'Body 1',
+  },
+  {
+    title: 'Item 2',
+    body: 'Body 2',
+  },
+]
+
 describe('Accordion', () => {
-  it.todo('should change index based on clicked AccordionItem')
-  it.todo('should show the correct body based on the current index')
+  afterEach(() => {
+    jest.clearAllMocks()
+  })
+
+  it('should render', () => {
+    render(<Accordion data={data} />)
+  })
+  it('should show the correct body based on clicked item', () => {
+    render(<Accordion data={data} />)
+    expect(screen.getByText('Body 2')).toHaveStyle('height: 0px')
+    fireEvent.click(screen.getByText('Item 2'))
+    expect(screen.getByText('Body 1')).toHaveStyle('height: 0px')
+  })
 })
