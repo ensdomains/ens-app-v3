c23727f06c3a68fc1ff7c703d31bf1dab575f8cd
Leon Talbert <leon.talbert@gmail.com>
2022-06-10 11:56:17 +0800

    resolver details test

--- src/components/pages/profile/[name]/details/MoreTab/ResolverDetails.test.tsx
+++ src/components/pages/profile/[name]/details/MoreTab/ResolverDetails.test.tsx
@@ -1,4 +1,59 @@
+import { render, screen } from '@app/test-utils'
+import { useRouter } from 'next/router'
+
+import { useProfile } from '@app/hooks/useProfile'
+
+import ResolverDetails from './ResolverDetails'
+
+jest.mock('next/router')
+jest.mock('@app/hooks/useProfile')
+
 describe('ResolverDetails', () => {
-  it.todo('should show green yes when resolver has been migrated')
-  it.todo('should show red no when resolver has not been migrated')
+  useRouter.mockImplementation(() => {
+    return {
+      query: {
+        name: 'nick.eth',
+      },
+    }
+  })
+
+  it('should display the address', () => {
+    const mockProfileResponse = {
+      profile: {
+        resolverAddress: 'address',
+        isMigrated: false,
+      },
+    }
+    useProfile.mockImplementation(() => {
+      return mockProfileResponse
+    })
+    render(<ResolverDetails />)
+    expect(screen.getByText('address'))
+  })
+  it('should show green yes when resolver has been migrated', () => {
+    const mockProfileResponse = {
+      profile: {
+        resolverAddress: 'address',
+        isMigrated: true,
+      },
+    }
+    useProfile.mockImplementation(() => {
+      return mockProfileResponse
+    })
+    render(<ResolverDetails />)
+    expect(screen.getByText('Yes')).toHaveStyle('color: rgb(73, 179, 147)')
+  })
+  it('should show red no when resolver has not been migrated', () => {
+    const mockProfileResponse = {
+      profile: {
+        resolverAddress: 'address',
+        isMigrated: false,
+      },
+    }
+    useProfile.mockImplementation(() => {
+      return mockProfileResponse
+    })
+    render(<ResolverDetails />)
+    expect(screen.getByText('No')).toHaveStyle('color: rgb(213,85,85)')
+  })
 })
