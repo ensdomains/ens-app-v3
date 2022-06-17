4aa9a95e5eabe4474175d10cd7b1a37438e7f150
storywithoutend <storywithoutend@gmail.com>
2022-06-17 11:19:21 +0800

    fix profile type

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,36 +1,3 @@
-export type DecodedContentHash = {
-  protocolType?: any
-  decoded?: any
-  error?: any
-}
+import type { ENS } from '@ensdomains/ensjs'
 
-export type Profile = {
-  isMigrated: boolean | null
-  createdAt: string | null
-  address?: string | undefined
-  name?: string | undefined
-  match?: boolean | undefined
-  message?: string | undefined
-  records?:
-    | {
-        contentHash?: string | DecodedContentHash | null | undefined
-        texts?:
-          | {
-              key: string | number
-              type: 'text' | 'addr' | 'contentHash'
-              coin?: string | undefined
-              value: string
-            }[]
-          | undefined
-        coinTypes?:
-          | {
-              key: string | number
-              type: 'text' | 'addr' | 'contentHash'
-              coin?: string | undefined
-              value: string
-            }[]
-          | undefined
-      }
-    | undefined
-  resolverAddress?: string | undefined
-}
+export type Profile = NonNullable<Awaited<ReturnType<ENS['getProfile']>>>
