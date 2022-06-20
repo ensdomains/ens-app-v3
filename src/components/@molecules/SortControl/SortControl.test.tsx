786e10b266366277ccb2fcad4dd64880d5605482
storywithoutend <storywithoutend@gmail.com>
2022-06-20 09:21:33 +0800

    fix svg test

--- src/components/@molecules/SortControl/SortControl.test.tsx
+++ src/components/@molecules/SortControl/SortControl.test.tsx
@@ -1,5 +1,9 @@
-import { render, screen, fireEvent, act } from '@testing-library/react'
-import SortControl, { SortValue, SortType, SortDirection } from './SortControl'
+import { render, screen, fireEvent } from '@testing-library/react'
+import { I18nextProvider } from 'react-i18next'
+import i18n from '@app/i18n'
+import { ThemeProvider } from 'styled-components'
+import { lightTheme } from '@ensdomains/thorin'
+import SortControl, { SortValue, SortDirection, SortType } from './SortControl'
 
 const sortValue: SortValue = {
   direction: SortDirection.desc,
@@ -16,22 +20,45 @@ describe('SortControl', () => {
   })
 
   it('should render', () => {
-    render(<SortControl value={sortValue} onChange={onChange} />)
+    render(
+      <I18nextProvider i18n={i18n}>
+        <ThemeProvider theme={lightTheme}>
+          <SortControl value={sortValue} onChange={onChange} />
+        </ThemeProvider>
+      </I18nextProvider>,
+    )
+    expect(screen.getByTestId('button-desc')).toBeInTheDocument()
+    expect(screen.getByTestId('button-asc')).toBeInTheDocument()
+    expect(screen.getByTestId('selected')).toBeInTheDocument()
   })
 
   it('should display correct values', () => {
-    render(<SortControl value={sortValue} onChange={onChange} />)
-    expect(screen.getByText('Expiry Date')).toBeVisible()
-    expect(screen.getByTestId('button-desc')).toHaveStyle(
-      'background-color: rbg(246, 246, 248)',
+    render(
+      <I18nextProvider i18n={i18n}>
+        <ThemeProvider theme={lightTheme}>
+          <SortControl value={sortValue} onChange={onChange} />
+        </ThemeProvider>
+      </I18nextProvider>,
     )
+    expect(screen.getByTestId('selected').textContent).toBe('Expiry Date')
+    const style = getComputedStyle(screen.getByTestId('button-desc'))
+    expect(style.filter).toBe('brightness(0.95)')
+    const style2 = getComputedStyle(screen.getByTestId('button-asc'))
+    expect(style2.filter).toBe('')
   })
 
   it('should emit onChange when values are changed', () => {
-    render(<SortControl value={sortValue} onChange={onChange} />)
-    act(() => {
-      fireEvent.click(screen.getByTestId('button-asc'))
-    })
+    render(
+      <I18nextProvider i18n={i18n}>
+        <ThemeProvider theme={lightTheme}>
+          <SortControl value={sortValue} onChange={onChange} />
+        </ThemeProvider>
+      </I18nextProvider>,
+    )
+    fireEvent.click(screen.getByTestId('button-asc'))
     expect(onChange.mock.calls[0][0].direction).toEqual('asc')
+    fireEvent.click(screen.getByTestId('selected'))
+    fireEvent.click(screen.getByText('Creation Date'))
+    expect(onChange.mock.calls[1][0].type).toEqual(SortType.creationDate)
   })
 })
