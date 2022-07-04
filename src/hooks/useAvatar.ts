2a6a8dd5c35a9d1ffdf5efec4d8fe4bff34bf6e4
tate <tate@ens.domains>
2022-07-04 12:31:05 +1000

    use fake img element for avatar load

--- src/hooks/useAvatar.ts
+++ src/hooks/useAvatar.ts
@@ -2,10 +2,36 @@ import { useEns } from '@app/utils/EnsProvider'
 import { ensNftImageUrl, imageUrlUnknownRecord } from '@app/utils/utils'
 import { useQuery } from 'react-query'
 
-export const useAvatar = (name: string | undefined, network: number) => {
-  const avatar = name ? imageUrlUnknownRecord(name, network) : undefined
+const fetchImg = async (url: string) =>
+  new Promise<string | undefined>((resolve) => {
+    const img = new Image()
+    img.src = url
+    const handleLoad = () => {
+      img.removeEventListener('load', handleLoad)
+      if (!img.complete) {
+        resolve(undefined)
+        return
+      }
+      if (!img.naturalWidth) {
+        resolve(undefined)
+        return
+      }
+      resolve(img.src)
+    }
 
-  return avatar
+    img.addEventListener('load', handleLoad)
+  })
+
+export const useAvatar = (name: string | undefined, network: number) => {
+  const { data, isLoading, status } = useQuery(
+    ['getAvatar', name],
+    () => fetchImg(imageUrlUnknownRecord(name!, network)),
+    {
+      enabled: !!name,
+    },
+  )
+
+  return { avatar: data, isLoading, status }
 }
 
 export const useNFTImage = (name: string | undefined, network: number) => {
@@ -23,11 +49,13 @@ export const useNFTImage = (name: string | undefined, network: number) => {
       staleTime: 60000,
     },
   )
+  const { data, isLoading, status } = useQuery(
+    ['getNFTImage', name],
+    () => fetchImg(ensNftImageUrl(name!, network, baseRegistrarAddress!)),
+    {
+      enabled: ready && !!name && !!baseRegistrarAddress && isCompatible,
+    },
+  )
 
-  const image =
-    name && baseRegistrarAddress
-      ? ensNftImageUrl(name, network, baseRegistrarAddress)
-      : undefined
-
-  return { image, isCompatible }
+  return { image: data, isLoading, status, isCompatible }
 }
