95ed1a4de810b3d59dc72dfd294c1246099982ca
tate <tate@ens.domains>
2022-07-07 19:09:07 +1000

    small height fix

--- src/components/@molecules/TransactionModal/TransactionModal.tsx
+++ src/components/@molecules/TransactionModal/TransactionModal.tsx
@@ -15,10 +15,12 @@ const InnerDialog = styled.div(
     display: flex;
     flex-direction: column;
     align-items: center;
-    justify-content: center;
+    justify-content: flex-start;
     width: ${theme.space.full};
     padding: 0 ${theme.space['5']};
     gap: ${theme.space['4']};
+    max-height: 60vh;
+    overflow-y: auto;
     ${mq.md.min(css`
       min-width: ${theme.space['128']};
     `)}
