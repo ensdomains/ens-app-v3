da6c24430eac7ade2bd1119ba6236bc3f23d401a
tate <tate@ens.domains>
2022-08-11 12:53:27 +1000

    added clean file

---
+++ e2e/clean.js
@@ -0,0 +1,30 @@
+import { ethers } from 'ethers'
+
+const provider = new ethers.providers.JsonRpcProvider('http://localhost:8545', 1337)
+
+const rpcSendBatch = (items) =>
+  fetch('http://localhost:8545', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(
+      items.map(({ method, params }, i) => ({
+        jsonrpc: '2.0',
+        method,
+        params,
+        id: i + 1,
+      })),
+    ),
+  })
+
+export const revert = async () => {
+  const currBlock = await provider.getBlockNumber()
+  await provider.send('evm_revert', [1])
+  await provider.send('evm_snapshot', [])
+  const revertBlock = await provider.getBlockNumber()
+  const blocksToMine = currBlock - revertBlock
+  await rpcSendBatch(
+    Array.from({ length: blocksToMine + 1 }, () => ({ method: 'evm_mine', params: [] })),
+  )
+}
