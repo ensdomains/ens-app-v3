8b4cbabeb4e197d904f33fb5aa149d75e3c268b3
tate <tate@ens.domains>
2022-08-15 15:58:30 +1000

    exclude test files

--- src/transaction-flow/input/index.tsx
+++ src/transaction-flow/input/index.tsx
@@ -7,13 +7,20 @@ import type { Props as ProfileEditorProps } from './ProfileEditor/ProfileEditor'
 import type { Props as SelectPrimaryNameProps } from './SelectPrimaryName'
 
 const dynamicHelper = <P,>(name: string) =>
-  dynamic<P>(() => import(name), { loading: () => <TransactionLoader /> })
+  dynamic<P>(
+    () =>
+      import(
+        /* webpackExclude: /\.test.tsx$/ */
+        `./${name}`
+      ),
+    { loading: () => <TransactionLoader /> },
+  )
 
-const EditResolver = dynamicHelper<EditResolverProps>('./EditResolver/EditResolver')
-const SelectPrimaryName = dynamicHelper<SelectPrimaryNameProps>('./SelectPrimaryName')
-const AdvancedEditor = dynamicHelper<AdvancedEditorProps>('./AdvancedEditor/AdvancedEditor')
-const ProfileEditor = dynamicHelper<ProfileEditorProps>('./ProfileEditor/ProfileEditor')
-const CreateSubname = dynamicHelper<CreateSubnameProps>('./CreateSubname')
+const EditResolver = dynamicHelper<EditResolverProps>('EditResolver/EditResolver')
+const SelectPrimaryName = dynamicHelper<SelectPrimaryNameProps>('SelectPrimaryName')
+const AdvancedEditor = dynamicHelper<AdvancedEditorProps>('AdvancedEditor/AdvancedEditor')
+const ProfileEditor = dynamicHelper<ProfileEditorProps>('ProfileEditor/ProfileEditor')
+const CreateSubname = dynamicHelper<CreateSubnameProps>('CreateSubname')
 
 export const DataInputComponents = {
   EditResolver,
