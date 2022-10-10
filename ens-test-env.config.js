a20860bed0f9f0e430830d4ce3f6e590c81c9c77
tate <tate@ens.domains>
2022-10-10 13:26:29 +1100

    add batchgatewayurls

--- ens-test-env.config.js
+++ ens-test-env.config.js
@@ -10,6 +10,9 @@ require('dotenv').config({
 
 process.env.ADDRESS_ETH_REGISTRAR = '0xc5a5C42992dECbae36851359345FE25997F5C42d'
 process.env.ADDRESS_NAME_WRAPPER = '0x9E545E3C0baAB3E08CdfD552C960A1050f373042'
+process.env.BATCH_GATEWAY_URLS = JSON.stringify([
+  'https://universal-offchain-unwrapper.ens-cf.workers.dev/',
+])
 
 /**
  * @type {import('@ensdomains/ens-test-env').ENSTestEnvConfig}
