ebda56ce80870c4c4a025f362444828474ceb8f2
Muhammed Tanrıkulu <md.tanrikulu@gmail.com>
2022-11-01 03:52:55 +0100

    remove comment

--- src/components/@molecules/NFTTemplate/NFTTemplate.test.tsx
+++ src/components/@molecules/NFTTemplate/NFTTemplate.test.tsx
@@ -49,7 +49,7 @@ describe('NFTTemplate', () => {
   })
 
   it('should use polyfill of Intl.Segmenter if browser does not support', async () => {
-    ;(window.Intl.Segmenter as typeof Intl['Segmenter']) = undefined as any // override native api
+    ;(window.Intl.Segmenter as typeof Intl['Segmenter']) = undefined as any
     const { getByText } = render(
       <NFTTemplate name="alisha.eth" backgroundImage={undefined} isNormalised />,
     )
