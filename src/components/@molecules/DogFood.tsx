20f58744db5b5384c2559833d1bc21edfb86c7e6
Leon Talbert <leon.talbert@gmail.com>
2022-11-08 19:19:44 +0800

    ...

---
+++ src/components/@molecules/DogFood.tsx
@@ -0,0 +1,118 @@
+import { ethers } from 'ethers'
+import { useEffect } from 'react'
+import { useForm } from 'react-hook-form'
+import { useTranslation } from 'react-i18next'
+import styled, { css } from 'styled-components'
+import { useQuery } from 'wagmi'
+
+import { Input, Typography, mq } from '@ensdomains/thorin'
+
+import { Spacer } from '@app/components/@atoms/Spacer'
+import { NameAvatar } from '@app/components/AvatarWithZorb'
+import { useChainId } from '@app/hooks/useChainId'
+import { usePrimary } from '@app/hooks/usePrimary'
+import { useEns } from '@app/utils/EnsProvider'
+
+const AvatarWrapper = styled.div(
+  ({ theme }) => css`
+    width: ${theme.space['7']};
+    min-width: ${theme.space['7']};
+    height: ${theme.space['7']};
+  `,
+)
+
+const ValueTypography = styled(Typography)(
+  () => css`
+    text-align: right;
+  `,
+)
+
+const ValueWithAvatarContainer = styled.div(
+  ({ theme }) => css`
+    display: flex;
+    flex-direction: row-reverse;
+    align-items: center;
+    justify-content: flex-end;
+    gap: ${theme.space['4']};
+    padding: 20px;
+    width: 100%;
+    border-radius: ${theme.radii.extraLarge};
+    border: ${theme.borderWidths.px} ${theme.borderStyles.solid}
+      rgba(${theme.shadesRaw.foreground}, 0.06);
+  `,
+)
+
+const InnerContainer = styled.div(() => [
+  css`
+    width: 100%;
+  `,
+  mq.sm.min(css`
+    width: 510px;
+  `),
+])
+
+const NameValue = ({ value }: { value: string }) => {
+  const network = useChainId()
+
+  return (
+    <ValueWithAvatarContainer>
+      <ValueTypography weight="bold">{value}</ValueTypography>
+      <AvatarWrapper>
+        <NameAvatar name={value} label={`${value}-avatar`} network={network} />
+      </AvatarWrapper>
+    </ValueWithAvatarContainer>
+  )
+}
+
+export const DogFood = (
+    { 
+      register, 
+      getFieldState, 
+      watch, 
+      setValue 
+    // eslint-disable-next-line prettier/prettier
+    }: Pick<ReturnType<typeof useForm<any>>, 'register' | 'getFieldState' | 'watch' | 'setValue'>
+) => {
+  const { t } = useTranslation('profile')
+  const { getRecords } = useEns()
+
+  const inputWatch = watch('dogfoodRaw')
+  const { data: ethNameAddress } = useQuery(
+    ['ethNameValidation', inputWatch],
+    async () => {
+      const result = await getRecords(inputWatch)
+      return result?.address
+    },
+    { enabled: inputWatch?.includes('.eth') },
+  )
+  const { name: primaryName } = usePrimary(ethNameAddress || inputWatch)
+
+  useEffect(() => {
+    setValue('address: ', ethNameAddress || inputWatch)
+  }, [ethNameAddress, inputWatch, setValue])
+
+  return (
+    <InnerContainer>
+      <Input
+        data-testid="send-name-input"
+        label="Send to"
+        placeholder={t('details.sendName.inputPlaceholder')}
+        {...register('dogfoodRaw', {
+          validate: {
+            length: (value) =>
+              !value.includes('.eth') && value.length !== 42
+                ? t('errors.addressLength')
+                : undefined,
+            isAddress: (value) =>
+              !value.includes('.eth') && !ethers.utils.isAddress(value)
+                ? t('errors.invalidAddress')
+                : undefined,
+          },
+        })}
+        error={getFieldState('dogfoodRaw').error?.message}
+      />
+      <Spacer $height="4" />
+      {primaryName && <NameValue value={primaryName} />}
+    </InnerContainer>
+  )
+}
--- src/transaction-flow/input/SendName-flow.tsx
+++ src/transaction-flow/input/SendName-flow.tsx
@@ -1,21 +1,15 @@
-import { ethers } from 'ethers'
-import { useEffect } from 'react'
 import { useForm } from 'react-hook-form'
 import { useTranslation } from 'react-i18next'
 import styled, { css } from 'styled-components'
-import { useAccount, useQuery } from 'wagmi'
+import { useAccount } from 'wagmi'
 
-import { Button, Checkbox, Dialog, Helper, Input, Typography, mq } from '@ensdomains/thorin'
+import { Button, Checkbox, Dialog, Helper, Typography } from '@ensdomains/thorin'
 
-import { Spacer } from '@app/components/@atoms/Spacer'
-import { NameAvatar } from '@app/components/AvatarWithZorb'
+import { DogFood } from '@app/components/@molecules/DogFood'
 import { Outlink } from '@app/components/Outlink'
 import { useBasicName } from '@app/hooks/useBasicName'
-import { useChainId } from '@app/hooks/useChainId'
-import { usePrimary } from '@app/hooks/usePrimary'
 import { useSelfAbilities } from '@app/hooks/useSelfAbilities'
 import { TransactionDialogPassthrough } from '@app/transaction-flow/types'
-import { useEns } from '@app/utils/EnsProvider'
 
 import { makeTransactionItem } from '../transaction'
 
@@ -27,7 +21,8 @@ type Data = {
 type FormData = {
   managerChoice: string
   ownerChoice: string
-  sendName: string
+  dogfoodRaw: string
+  address: string
 }
 
 export type Props = {
@@ -35,35 +30,6 @@ export type Props = {
 } & TransactionDialogPassthrough
 
 type BasicNameData = ReturnType<typeof useBasicName>
-
-const AvatarWrapper = styled.div(
-  ({ theme }) => css`
-    width: ${theme.space['7']};
-    min-width: ${theme.space['7']};
-    height: ${theme.space['7']};
-  `,
-)
-
-const ValueTypography = styled(Typography)(
-  () => css`
-    text-align: right;
-  `,
-)
-
-const ValueWithAvatarContainer = styled.div(
-  ({ theme }) => css`
-    display: flex;
-    flex-direction: row-reverse;
-    align-items: center;
-    justify-content: flex-end;
-    gap: ${theme.space['4']};
-    padding: 20px;
-    width: 100%;
-    border-radius: ${theme.radii.extraLarge};
-    border: ${theme.borderWidths.px} ${theme.borderStyles.solid}
-      rgba(${theme.shadesRaw.foreground}, 0.06);
-  `,
-)
 
 const SwitchBox = styled.div(
   ({ theme }) => css`
@@ -83,73 +49,6 @@ const TextContainer = styled.div`
   display: flex;
   flex-direction: column;
 `
-
-const InnerContainer = styled.div(() => [
-  css`
-    width: 100%;
-  `,
-  mq.sm.min(css`
-    width: 510px;
-  `),
-])
-
-const NameValue = ({ value }: { value: string }) => {
-  const network = useChainId()
-
-  return (
-    <ValueWithAvatarContainer>
-      <ValueTypography weight="bold">{value}</ValueTypography>
-      <AvatarWrapper>
-        <NameAvatar name={value} label={`${value}-avatar`} network={network} />
-      </AvatarWrapper>
-    </ValueWithAvatarContainer>
-  )
-}
-
-const DogFood = ({ register, getFieldState, watch, setValue }) => {
-  const { t } = useTranslation('profile')
-  const { getRecords } = useEns()
-
-  const inputWatch = watch('dogfoodRaw')
-  const { data: ethNameAddress } = useQuery(
-    ['ethNameValidation', inputWatch],
-    async () => {
-      const result = await getRecords(inputWatch)
-      return result?.address
-    },
-    { enabled: inputWatch?.includes('.eth') },
-  )
-  const { name: primaryName } = usePrimary(ethNameAddress || inputWatch)
-
-  useEffect(() => {
-    setValue('address: ', ethNameAddress || inputWatch)
-  }, [ethNameAddress, inputWatch, setValue])
-
-  return (
-    <InnerContainer>
-      <Input
-        data-testid="send-name-input"
-        label="Send to"
-        placeholder={t('details.sendName.inputPlaceholder')}
-        {...register('dogfoodRaw', {
-          validate: {
-            length: (value) =>
-              !value.includes('.eth') && value.length !== 42
-                ? t('errors.addressLength')
-                : undefined,
-            isAddress: (value) =>
-              !value.includes('.eth') && !ethers.utils.isAddress(value)
-                ? t('errors.invalidAddress')
-                : undefined,
-          },
-        })}
-        error={getFieldState('dogfoodRaw').error?.message}
-      />
-      <Spacer $height="4" />
-      {primaryName && <NameValue value={primaryName} />}
-    </InnerContainer>
-  )
-}
 
 export const handleSubmitForm =
   ({
@@ -246,8 +145,7 @@ export const SendName = ({ data, dispatch, onDismiss }: Props) => {
     name,
   )
 
-  const onSubmit = (formData) => {
-    console.log(formData)
+  const onSubmit = (formData: any) => {
     handleSubmitForm({
       basicNameData,
       dispatch,
