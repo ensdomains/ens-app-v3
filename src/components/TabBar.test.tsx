13c5473e65c966f0d9c4fd71cd58da3ccf5caaa6
storywithoutend <storywithoutend@gmail.com>
2022-11-09 12:56:06 +0800

    update tests

--- src/components/TabBar.test.tsx
+++ src/components/TabBar.test.tsx
@@ -4,38 +4,27 @@ import { useAccount } from 'wagmi'
 
 import { useRecentTransactions } from '@app/hooks/transactions/useRecentTransactions'
 import { useActiveRoute } from '@app/hooks/useActiveRoute'
+import { useInitial } from '@app/hooks/useInitial'
 
 import { TabBar } from './TabBar'
 
 jest.mock('wagmi')
 jest.mock('@app/hooks/transactions/useRecentTransactions')
 jest.mock('@app/hooks/useActiveRoute')
+jest.mock('@app/hooks/useInitial')
 
 const mockUseAccount = mockFunction(useAccount)
 const mockUseActiveRoute = mockFunction(useActiveRoute)
 const mockUseRecentTransactions = mockFunction(useRecentTransactions)
+const mockUseInitial = mockFunction(useInitial)
 
 describe('TabBar', () => {
   mockUseActiveRoute.mockReturnValue('profile')
   mockUseRecentTransactions.mockReturnValue([])
+  mockUseAccount.mockReturnValue({ address: '0x123' })
 
-  it('should return null if useAccount is connecting', () => {
-    mockUseAccount.mockReturnValue({
-      address: undefined,
-      isConnecting: true,
-      isReconnecting: false,
-    })
-
-    const { container } = render(<TabBar />)
-    expect(container).toBeEmptyDOMElement()
-  })
-
-  it('should return null if useAccount is reconnecting', () => {
-    mockUseAccount.mockReturnValue({
-      address: undefined,
-      isConnecting: false,
-      isReconnecting: true,
-    })
+  it('should return null if useInitial is true', () => {
+    mockUseInitial.mockReturnValue(true)
 
     const { container } = render(<TabBar />)
     expect(container).toBeEmptyDOMElement()
