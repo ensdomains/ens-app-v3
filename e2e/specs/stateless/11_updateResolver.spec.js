99eeba1a18d5d7d154a49591f2e1e7d189da7877
Leon Talbert <leon.talbert@gmail.com>
2022-11-16 15:35:45 +0800

    cypress fix

--- e2e/specs/stateless/11_updateResolver.spec.js
+++ e2e/specs/stateless/11_updateResolver.spec.js
@@ -1,7 +1,7 @@
 import { acceptMetamaskAccess } from '../../setup'
 
-const newResolver = '0x84eA74d481Ee0A5332c457a4d796187F6Ba67fEB'
-const oldResolver = '0x70e0bA845a1A0F2DA3359C97E0285013525FFC49'
+const newResolver = '0x70e0bA845a1A0F2DA3359C97E0285013525FFC49'
+const oldResolver = '0x84eA74d481Ee0A5332c457a4d796187F6Ba67fEB'
 
 describe('Update Resolver', () => {
   before(() => {
@@ -9,22 +9,11 @@ describe('Update Resolver', () => {
   })
 
   describe('Happy', () => {
-    describe('When profile is not updated to latest resolver', () => {
-      it('should allow user to update if they have chosen to use the latest resolver', () => {
+    describe('When profile is updated to latest resolver', () => {
+      it('should disable the latest resolver button, have custom resolver checked, and allow user to change reslover address', () => {
         cy.visit(
           '/profile/wrapped.eth/details?from=%2Fprofile%2Fwrapped.eth%3Ffrom%3D%252Fmy%252Fnames&name=wrapped.eth&tab=advanced',
         )
-        cy.findByTestId('accordion-resolverDetails-edit').click()
-        cy.findByTestId('update-button').click()
-        cy.findByTestId('transaction-modal-confirm-button').click()
-        cy.confirmMetamaskTransaction()
-        cy.findByTestId('transaction-modal-complete-button').click()
-        cy.findByTestId('resolver-address').should('have.text', newResolver)
-      })
-    })
-
-    describe('When profile is updated to latest resolver', () => {
-      it('should disable the latest resolver button, have custom resolver checked, and allow user to change reslover address', () => {
         cy.findByTestId('accordion-resolverDetails-edit').click()
         cy.findByTestId('latest-resolver-radio').should('be.disabled')
         cy.findByTestId('custom-resolver-radio').should('not.be.disabled')
@@ -33,6 +22,25 @@ describe('Update Resolver', () => {
         cy.findByTestId('transaction-modal-confirm-button').click()
         cy.confirmMetamaskTransaction()
         cy.findByTestId('transaction-modal-complete-button').click()
+        cy.findByTestId('resolver-address').should('have.text', oldResolver)
+      })
+    })
+
+    describe('When profile is not updated to latest resolver', () => {
+      it('should allow user to update if they have chosen to use the latest resolver', () => {
+        cy.findByTestId('accordion-resolverDetails-edit').click()
+        cy.findByTestId('update-button').click()
+        cy.findByTestId('transaction-modal-confirm-button').click()
+        cy.confirmMetamaskTransaction()
+        cy.findByTestId('transaction-modal-complete-button').click()
+
+        //This is only needed on cypress, not sure why!
+        cy.findByTestId('accordion-resolverDetails-edit').click()
+        cy.findByTestId('update-button').click()
+        cy.findByTestId('transaction-modal-confirm-button').click()
+        cy.confirmMetamaskTransaction()
+        cy.findByTestId('transaction-modal-complete-button').click()
+
         cy.findByTestId('resolver-address').should('have.text', newResolver)
       })
     })
