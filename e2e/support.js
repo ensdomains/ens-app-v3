e366385146558cf33be6eeaf47c97bf16013156a
storywithoutend <storywithoutend@gmail.com>
2022-11-23 18:17:37 +0800

    test out logging with state.selectedKey as Dialog open control

--- e2e/support.js
+++ e2e/support.js
@@ -4,7 +4,10 @@ import 'cypress-localstorage-commands'
 
 beforeEach(() => {
   cy.restoreLocalStorage()
+  console.log('beforeEach txFlow', localStorage.getItem( 'tx-flow'))
 })
+
 afterEach(() => {
   cy.saveLocalStorage()
+  console.log('beforeEach txFlow', localStorage.getItem( 'tx-flow'))
 })
