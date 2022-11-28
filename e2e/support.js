58b2da56a8a48131306a6697cc034b8cbc820f46
storywithoutend <storywithoutend@gmail.com>
2022-11-28 16:11:31 +0800

    clean console log

--- e2e/support.js
+++ e2e/support.js
@@ -4,10 +4,8 @@ import 'cypress-localstorage-commands'
 
 beforeEach(() => {
   cy.restoreLocalStorage()
-  console.log('beforeEach txFlow', localStorage.getItem( 'tx-flow'))
 })
 
 afterEach(() => {
   cy.saveLocalStorage()
-  console.log('beforeEach txFlow', localStorage.getItem( 'tx-flow'))
 })
