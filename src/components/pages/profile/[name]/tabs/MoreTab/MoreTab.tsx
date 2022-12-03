293ab4e46b2b6e5d8b6e1010aa5290e7b564436f
Leon Talbert <leon.talbert@gmail.com>
2022-12-03 08:43:39 +0800

    fix unit tests

--- src/components/pages/profile/[name]/tabs/MoreTab/MoreTab.tsx
+++ src/components/pages/profile/[name]/tabs/MoreTab/MoreTab.tsx
@@ -11,24 +11,6 @@ import Ownership from './Ownership'
 import Resolver from './Resolver'
 import Token from './Token'
 
-// export const TokenId = () => {
-//   const { t } = useTranslation('profile')
-//   const router = useRouter()
-//   const { name } = router.query
-
-//   const label = (name as string)?.split('.')?.[0]
-//   const labelHash = utils.keccak256(utils.toUtf8Bytes(label) || '')
-//   const tokenId = BigNumber.from(labelHash).toString()
-
-//   return (
-//     <>
-//       <RecordItem itemKey={t('details.tabs.advanced.tokenId.hex')} value={labelHash} />
-//       <div style={{ height: 10 }} />
-//       <RecordItem itemKey={t('details.tabs.advanced.tokenId.decimal')} value={tokenId} />
-//     </>
-//   )
-// }
-
 const MoreContainer = styled(CacheableComponent)(
   ({ theme }) => css`
     display: flex;
@@ -41,43 +23,6 @@ const MoreContainer = styled(CacheableComponent)(
   `,
 )
 
-// export const generateAccordionData = (
-//   wrapperData: ReturnType<typeof useGetWrapperData>['wrapperData'],
-//   t: TFunction,
-//   ownerData: ReturnType<typeof useBasicName>['ownerData'],
-//   isWrapped: boolean,
-//   name: string,
-//   address?: string,
-// ): AccordionData[] => [
-//   {
-//     title: t('details.tabs.advanced.resolver.label'),
-//     body: ResolverDetails,
-//     name: 'resolverDetails',
-//     canEdit: ownerData?.owner === address,
-//   },
-//   {
-//     title: t('details.tabs.advanced.fuses.label'),
-//     body: Fuses,
-//     disabled: !wrapperData || wrapperData?.owner === emptyAddress,
-//     name: 'fuses',
-//     canEdit: ownerData?.owner === address && isWrapped,
-//   },
-//   {
-//     title: t('details.tabs.advanced.tokenId.label'),
-//     body: TokenId,
-//     name: 'tokenId',
-//   },
-//   ...(name.split('.').length === 2 && name.startsWith('.eth')
-//     ? [
-//         {
-//           title: t('details.tabs.advanced.registrationDate.label'),
-//           body: RegistrationDate,
-//           name: 'registrationDate',
-//         },
-//       ]
-//     : []),
-// ]
-
 type Props = {
   name: string
   nameDetails: ReturnType<typeof useNameDetails>
@@ -85,12 +30,6 @@ type Props = {
 }
 
 const MoreTab = ({ name, nameDetails, selfAbilities }: Props) => {
-  // const { t } = useTranslation('profile')
-  // const router = useRouter()
-  // const { name } = router.query
-  // const { wrapperData } = useGetWrapperData((name as string) || '')
-  // const { address } = useAccount()
-  // const { ownerData, isWrapped } = useBasicName(name as string)
   const {
     ownerData,
     wrapperData,
---
+++ src/components/pages/profile/[name]/tabs/MoreTab/Token.test.tsx
@@ -0,0 +1,39 @@
+import { mockFunction, render, screen } from '@app/test-utils'
+
+import { BigNumber, utils } from 'ethers'
+
+import { useChainId } from '@app/hooks/useChainId'
+
+import Token from './Token'
+
+jest.mock('next/router')
+jest.mock('@app/hooks/useChainId')
+
+const mockUseChainId = mockFunction(useChainId)
+
+describe('Token', () => {
+  describe('TokenId', () => {
+    it('should display the correct tokenID (decimal and hex) for an unwrapped names', () => {
+      mockUseChainId.mockReturnValue(1)
+      const name = 'nick.eth'
+      const label = 'nick'
+      const labelhash = utils.keccak256(utils.toUtf8Bytes(label))
+      const tokenId = BigNumber.from(labelhash).toString()
+
+      render(<Token {...{ name, isWrapped: false }} />)
+      expect(screen.getByText(labelhash)).toBeVisible()
+      expect(screen.getByText(tokenId)).toBeVisible()
+    })
+
+    it('should display the correct tokenID (decimal and hex) for an wrapped names', () => {
+      mockUseChainId.mockReturnValue(1)
+      const name = 'nick.eth'
+      const namehash = utils.namehash(name)
+      const tokenId = BigNumber.from(namehash).toString()
+
+      render(<Token {...{ name, isWrapped: true }} />)
+      expect(screen.getByText(namehash)).toBeVisible()
+      expect(screen.getByText(tokenId)).toBeVisible()
+    })
+  })
+})
