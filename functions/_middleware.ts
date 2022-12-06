e1fb603a785184d293cd30dc6d11b0490551fb82
tate <tate@ens.domains>
2022-12-06 11:31:57 +1100

    fixed og share info

--- functions/_middleware.ts
+++ functions/_middleware.ts
@@ -1,9 +1,25 @@
+import { normalise } from '@ensdomains/ensjs/utils/normalise'
+
+class ContentModifier {
+  private newContent: string
+
+  constructor(newContent: string) {
+    this.newContent = newContent
+  }
+
+  element(element: Element) {
+    element.setInnerContent(this.newContent)
+  }
+}
+
 export const onRequest: PagesFunction = async ({ request, next }) => {
   const url = new URL(request.url)
 
   const paths = url.pathname.split('/')
 
   let rewrite = false
+  let callback: ((res: Response) => Response) | null = null
+
   if (paths[1] === 'my' && paths[2] === 'profile') {
     url.pathname = '/profile'
     url.searchParams.set('connected', 'true')
@@ -12,14 +28,32 @@ export const onRequest: PagesFunction = async ({ request, next }) => {
     url.pathname = '/my/names'
     url.searchParams.set('address', paths[2])
     rewrite = true
-  } else if (paths[1] === 'profile' && !!paths[2] && paths[3] === 'details') {
-    url.pathname = '/profile/details'
-    url.searchParams.set('name', paths[2])
-    rewrite = true
   } else if (paths[1] === 'profile' && !!paths[2]) {
     url.pathname = '/profile'
     url.searchParams.set('name', paths[2])
     rewrite = true
+
+    const decodedName = decodeURIComponent(paths[2])
+    let normalisedName: string | null = null
+    try {
+      normalisedName = normalise(decodedName)
+      // eslint-disable-next-line no-empty
+    } catch {}
+    callback = (res) =>
+      new HTMLRewriter()
+        .on(
+          'title',
+          new ContentModifier(normalisedName ? `${normalisedName} on ENS` : 'Invalid Name - ENS'),
+        )
+        .on(
+          'meta[name="description"]',
+          new ContentModifier(
+            normalisedName
+              ? `${normalisedName}'s profile on the Ethereum Name Service`
+              : `An error occurred`,
+          ),
+        )
+        .transform(res)
   } else if (paths[1] === 'import' && !!paths[2]) {
     url.pathname = '/import'
     url.searchParams.set('name', paths[2])
@@ -37,6 +71,11 @@ export const onRequest: PagesFunction = async ({ request, next }) => {
   if (rewrite) {
     const rewriteRequest = new Request(url.toString(), request)
     const response = await fetch(rewriteRequest)
+
+    if (callback) {
+      return callback(response)
+    }
+
     return response
   }
 
