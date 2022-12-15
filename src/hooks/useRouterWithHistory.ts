8862976eed17a1f4523f33d335757768a789e9f4
tate <tate@ens.domains>
2022-12-16 10:55:24 +1100

    provide as to router.push

--- src/hooks/useRouterWithHistory.ts
+++ src/hooks/useRouterWithHistory.ts
@@ -14,7 +14,7 @@ export const useRouterWithHistory = () => {
     const initialQuery = query || {}
     initialQuery.from = router.asPath
     const destination = getDestination({ pathname, query: initialQuery })
-    router.push(destination)
+    router.push(destination, typeof destination === 'string' ? undefined : pathname)
   }
 
   return { ...router, push, pushWithHistory }
