c2879117258659f479307c161369c318bee03cff
tate <tate@ens.domains>
2023-01-11 16:02:55 +1100

    use confirm permission to spend

--- e2e/specs/stateless/09_wrapName.spec.js
+++ e2e/specs/stateless/09_wrapName.spec.js
@@ -69,4 +69,31 @@ describe('Wrap Name', () => {
     cy.findByTestId('transaction-modal-inner').should('not.exist')
     cy.findByTestId('wrapper-cta-container').should('not.exist')
   })
+  it('should allow wrapping a subdomain', () => {
+    cy.visit('/profile/sub.unwrapped-with-wrapped-subnames.eth')
+    connectFromExisting()
+    cy.findByTestId('wrapper-cta-container').should('be.visible')
+    cy.findByTestId('wrapper-cta-button').should('contain.text', 'Upgrade')
+
+    // should approve name wrapper for address
+    cy.findByTestId('wrapper-cta-button').click()
+    cy.findByTestId('transaction-modal-inner').should('be.visible')
+    cy.findByTestId('transaction-dialog-intro-trailing-btn').click()
+    cy.findByTestId('transaction-modal-confirm-button').click()
+    cy.log('Confirm metamask transaction for name wrapper approval')
+    cy.confirmMetamaskPermissionToSpend()
+    cy.findByTestId('transaction-modal-complete-button').click()
+
+    // should wrap the name
+    // arbitrary wait so metamask updates the account nonce
+    cy.wait(1000)
+    cy.findByTestId('transaction-modal-confirm-button').click()
+    cy.log('Confirm metamask transaction for wrapping name')
+    cy.confirmMetamaskTransaction()
+    cy.findByTestId('transaction-modal-complete-button').click()
+
+    // should remove the notification once the name is wrapped
+    cy.findByTestId('transaction-modal-inner').should('not.exist')
+    cy.findByTestId('wrapper-cta-container').should('not.exist')
+  })
 })
