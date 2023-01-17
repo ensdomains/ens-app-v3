cb6a83c39fd09786e95b58d88af88d7089adc5d3
storywithoutend <storywithoutend@gmail.com>
2023-01-17 22:04:02 +0800

    update toast close icon

--- e2e/specs/stateless/09_wrapName.spec.js
+++ e2e/specs/stateless/09_wrapName.spec.js
@@ -50,7 +50,7 @@ describe('Wrap Name', () => {
     cy.findByTestId('transaction-modal-confirm-button').click()
     cy.confirmMetamaskTransaction()
     cy.findByTestId('transaction-modal-complete-button').click()
-    cy.findByTestId('close-icon').click()
+    cy.findByTestId('toast-close-icon').click()
     cy.findByTestId('wrapper-cta-button').should('contain.text', 'Resume Upgrade')
 
     // should open to correctly resumed state
