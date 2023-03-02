b6e8965189b58880be3a994a43baeb66c7caab10
storywithoutend <storywithoutend@gmail.com>
2023-03-02 18:08:21 +0800

    update time utils

--- src/utils/utils.spec.ts
+++ src/utils/utils.spec.ts
@@ -39,6 +39,6 @@ describe('formatExpiry', () => {
   it('should format the date as expected', () => {
     const expiry = new Date('2020-01-01')
     const result = formatExpiry(expiry)
-    expect(result).toEqual('\nJanuary 1, 2020')
+    expect(result).toEqual('January 1, 2020')
   })
 })
