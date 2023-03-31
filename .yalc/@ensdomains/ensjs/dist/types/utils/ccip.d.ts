bd43b91875cc23e2435104713021b6a7bb9cd45d
tate <tate@ens.domains>
2023-03-31 11:41:12 +1100

    use remote ensjs

--- .yalc/@ensdomains/ensjs/dist/types/utils/ccip.d.ts
+++
@@ -1,3 +0,0 @@
-import type { BaseProvider, TransactionRequest } from '@ethersproject/providers';
-declare const ccipLookup: (provider: BaseProvider, transaction: TransactionRequest, result: string) => Promise<string | undefined>;
-export default ccipLookup;
--- .yalc/@ensdomains/ensjs/src/contracts/publicResolver.ts
+++
@@ -1,5 +0,0 @@
-import type { JsonRpcProvider } from '@ethersproject/providers'
-import { PublicResolver__factory } from '../generated/factories/PublicResolver__factory'
-
-export default (provider: JsonRpcProvider, address: string) =>
-  PublicResolver__factory.connect(address, provider)
