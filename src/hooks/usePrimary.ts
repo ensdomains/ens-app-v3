3b972b0a30ff15dfbbd702f0279fafe3a4c16088
storywithoutend <storywithoutend@gmail.com>
2023-04-06 17:03:46 +0800

    add log for testing

--- src/hooks/usePrimary.ts
+++ src/hooks/usePrimary.ts
@@ -21,6 +21,7 @@ export const usePrimary = (address: string, skip?: any): Result => {
     ['getName', address],
     async () => {
       const res = await getName(address)
+      console.log('getName', res)
       if (!res || !res.name || !res.match) return null
       return {
         ...res,
