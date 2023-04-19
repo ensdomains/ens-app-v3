e80478da43c0d9cccb89e9052033cc16ceea59fc
tate <tate@ens.domains>
2023-04-19 11:10:42 +1000

    feat: tenderly registration gas estimates

--- src/components/pages/profile/[name]/registration/steps/Transactions.tsx
+++ src/components/pages/profile/[name]/registration/steps/Transactions.tsx
@@ -3,8 +3,6 @@ import { useTranslation } from 'react-i18next'
 import styled, { css } from 'styled-components'
 import { useAccount } from 'wagmi'
 
-import { ChildFuses } from '@ensdomains/ensjs'
-import { BaseRegistrationParams } from '@ensdomains/ensjs/utils/registerHelpers'
 import {
   AlertSVG,
   Button,
@@ -20,12 +18,11 @@ import { InnerDialog } from '@app/components/@atoms/InnerDialog'
 import MobileFullWidth from '@app/components/@atoms/MobileFullWidth'
 import { Card } from '@app/components/Card'
 import { useNameDetails } from '@app/hooks/useNameDetails'
+import useRegistrationParams from '@app/hooks/useRegistrationParams'
 import { useTransactionFlow } from '@app/transaction-flow/TransactionFlowProvider'
 import { makeTransactionItem } from '@app/transaction-flow/transaction'
-import { yearsToSeconds } from '@app/utils/utils'
 
 import { RegistrationReducerDataItem } from '../types'
-import { profileRecordsToRecordOptions } from './Profile/profileRecordUtils'
 
 const StyledCard = styled(Card)(
   ({ theme }) => css`
@@ -155,29 +152,11 @@ const Transactions = ({ registrationData, nameDetails, callback, onStart }: Prop
     commitTimestamp && commitTimestamp + 60000 < Date.now(),
   )
 
-  const registrationParams: BaseRegistrationParams & { name: string } = useMemo(
-    () => ({
-      name: nameDetails.normalisedName,
-      owner: address!,
-      duration: yearsToSeconds(registrationData.years),
-      resolverAddress: registrationData.resolver,
-      secret: registrationData.secret,
-      records: profileRecordsToRecordOptions(
-        registrationData.records,
-        registrationData.clearRecords,
-      ),
-      fuses: {
-        named: registrationData.permissions
-          ? (Object.keys(registrationData.permissions).filter(
-              (key) => !!registrationData.permissions?.[key as ChildFuses['fuse']],
-            ) as ChildFuses['fuse'][])
-          : [],
-        unnamed: [],
-      },
-      reverseRecord: registrationData.reverseRecord,
-    }),
-    [address, nameDetails, registrationData],
-  )
+  const registrationParams = useRegistrationParams({
+    name: nameDetails.normalisedName,
+    owner: address!,
+    registrationData,
+  })
 
   const makeCommitNameFlow = useCallback(() => {
     onStart()
