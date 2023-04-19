0c5eec944b349f03ab7ff22537e4426e13ef5f66
tate <tate@ens.domains>
2023-04-19 14:02:54 +1000

    fix: disable unused sentry replay

--- sentry.client.config.js
+++ sentry.client.config.js
@@ -10,11 +10,6 @@ Sentry.init({
   // Adjust this value in production, or use tracesSampler for greater control
   tracesSampleRate: 1.0,
   enabled: !(process.env.NODE_ENV === 'development' || process.env.NEXT_PUBLIC_PROVIDER),
-  replaysSessionSampleRate: process.env.NODE_ENV === 'development' ? 1 : 0.1,
-  // If the entire session is not sampled, use the below sample rate to sample
-  // sessions when an error occurs.
-  replaysOnErrorSampleRate: 1.0,
-  integrations: [new Sentry.Replay()],
   // ...
   // Note: if you want to override the automatic release value, do not set a
   // `release` value here - use the environment variable `SENTRY_RELEASE`, so
