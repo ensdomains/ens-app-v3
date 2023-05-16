cd0cd8250b53bd5a45789c8e1c915ac40dae1999
storywithoutend <storywithoutend@gmail.com>
2023-05-16 20:18:10 +0800

    refactor to components + useMutation

--- src/components/pages/profile/[name]/tabs/MoreTab/Miscellaneous.tsx
+++ src/components/pages/profile/[name]/tabs/MoreTab/Miscellaneous.tsx
@@ -1,20 +1,15 @@
 import { CalendarEvent, google, ics, office365, outlook, yahoo } from 'calendar-link'
 import { useCallback, useState } from 'react'
-import { useForm } from 'react-hook-form'
-import { Trans, useTranslation } from 'react-i18next'
+import { useTranslation } from 'react-i18next'
 import styled, { css } from 'styled-components'
-import { match } from 'ts-pattern'
 import { useAccount } from 'wagmi'
 
-import { Button, Dialog, Dropdown, Input, Typography, mq } from '@ensdomains/thorin'
+import { Button, Dropdown, Typography, mq } from '@ensdomains/thorin'
 
 import CalendarSVG from '@app/assets/Calendar.svg'
 import FastForwardSVG from '@app/assets/FastForward.svg'
 import OutlinkSVG from '@app/assets/Outlink.svg'
 import { cacheableComponentStyles } from '@app/components/@atoms/CacheableComponent'
-import { Spacer } from '@app/components/@atoms/Spacer'
-import { Outlink } from '@app/components/Outlink'
-import { useChainId } from '@app/hooks/useChainId'
 import { useChainName } from '@app/hooks/useChainName'
 import useRegistrationDate from '@app/hooks/useRegistrationData'
 import { useSelfAbilities } from '@app/hooks/useSelfAbilities'
@@ -22,16 +17,7 @@ import { useTransactionFlow } from '@app/transaction-flow/TransactionFlowProvide
 import { formatDateTime, formatExpiry, makeEtherscanLink } from '@app/utils/utils'
 
 import { TabWrapper } from '../../../TabWrapper'
-
-interface FormData {
-  email: string
-}
-
-const EARNIFI_ENDPOINT = 'https://notifications-api.vercel.app/api/v1/ens/subscribe'
-const EARNIFI_OUTLINK =
-  'https://earni.fi/?utm_source=ENS+Modal&utm_medium=Banner&utm_campaign=ENS_Partnership'
-
-const emailRegex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,63}$/i
+import { EarnifiDialog } from './EarnifiDialog'
 
 const calendarOptions = [
   {
@@ -162,15 +148,6 @@ const ButtonContainer = styled.div(
   `,
 )
 
-const Form = styled.form(
-  ({ theme }) => css`
-    width: 100%;
-    ${mq.sm.min(css`
-      width: ${theme.space['128']};
-    `)}
-  `,
-)
-
 const Miscellaneous = ({
   name,
   expiryDate,
@@ -181,12 +158,9 @@ const Miscellaneous = ({
   isCachedData: boolean
 }) => {
   const { t } = useTranslation('common')
-  const [hasEarnifiDialog, setHasEarnifiDialog] = useState(false)
-  const [hasCreatedReminder, setHasCreatedReminder] = useState(false)
 
   const { address } = useAccount()
   const chainName = useChainName()
-  const chainId = useChainId()
   const { data: registrationData, isCachedData: registrationCachedData } = useRegistrationDate(name)
   const { canExtend, canEdit } = useSelfAbilities(address, name)
 
@@ -203,127 +177,17 @@ const Miscellaneous = ({
     [name, expiryDate, t],
   )
 
-  const {
-    register,
-    handleSubmit,
-    setError,
-    clearErrors,
-    formState: { errors, isSubmitting },
-  } = useForm<FormData>({ mode: 'onChange' })
-
-  const onSubmit = async ({ email }: FormData) => {
-    try {
-      const response = await fetch(EARNIFI_ENDPOINT, {
-        method: 'POST',
-        headers: {
-          // eslint-disable-next-line @typescript-eslint/naming-convention
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          email,
-          address: name,
-          chainId,
-        }),
-      })
-      if (response?.ok) {
-        setHasCreatedReminder(true)
-        return
-      }
-      if (response?.status === 400) {
-        const responseMessage = ((await response.json()) as any)?.message
-        setError('email', {
-          type: 'submitError',
-          message: responseMessage || t('tabs.more.misc.earnfi.submitError', { ns: 'profile' }),
-        })
-        return
-      }
-      throw new Error('Submission error')
-    } catch (e) {
-      setError('email', {
-        type: 'submitError',
-        message: t('tabs.more.misc.earnfi.submitError', { ns: 'profile' }),
-      })
-    }
-    setTimeout(() => {
-      clearErrors('email')
-    }, 3000)
-  }
+  const [showEarnifiDialog, setShowEarnifiDialog] = useState(false)
 
   if (!expiryDate) return null
 
   return (
     <>
-      <Dialog open={hasEarnifiDialog} variant="blank" onDismiss={() => null}>
-        <Dialog.Heading title={t('tabs.more.misc.earnfi.title', { ns: 'profile' })} />
-        {match(hasCreatedReminder)
-          .with(false, () => (
-            <Form onSubmit={handleSubmit(onSubmit)}>
-              <Typography style={{ textAlign: 'center' }}>
-                <Trans
-                  style={{ textAlign: 'center' }}
-                  i18nKey="tabs.more.misc.earnfi.enterEmail"
-                  ns="profile"
-                  components={{
-                    a: <Outlink href={EARNIFI_OUTLINK} />,
-                  }}
-                />
-              </Typography>
-              <Spacer $height="3" />
-              <Input
-                type="email"
-                id="email"
-                label={t('action.enterEmail')}
-                {...register('email', {
-                  required: t('errors.emailRequired'),
-                  pattern: {
-                    value: emailRegex,
-                    message: t('errors.emailInvalid'),
-                  },
-                })}
-                error={errors.email?.message}
-              />
-              <Spacer $height="3" />
-              <Dialog.Footer
-                leading={
-                  <Button
-                    onClick={() => {
-                      setHasEarnifiDialog(false)
-                      setHasCreatedReminder(false)
-                    }}
-                    colorStyle="accentSecondary"
-                  >
-                    {t('action.cancel')}
-                  </Button>
-                }
-                trailing={
-                  <Button type="submit" disabled={!!errors.email} loading={isSubmitting}>
-                    {t('action.continue')}
-                  </Button>
-                }
-              />
-            </Form>
-          ))
-          .with(true, () => (
-            <>
-              <div style={{ width: '500px', textAlign: 'center' }}>
-                {t('tabs.more.misc.earnfi.emailConfirmation', { ns: 'profile' })}
-              </div>
-              <Dialog.Footer
-                trailing={
-                  <Button
-                    onClick={() => {
-                      setHasEarnifiDialog(false)
-                      setHasCreatedReminder(false)
-                    }}
-                  >
-                    {t('action.close')}
-                  </Button>
-                }
-              />
-            </>
-          ))
-          .exhaustive()}
-      </Dialog>
+      <EarnifiDialog
+        name={name}
+        open={showEarnifiDialog}
+        onDismiss={() => setShowEarnifiDialog(false)}
+      />
       <MiscellaneousContainer $isCached={isCachedData || registrationCachedData}>
         <DatesContainer>
           {registrationData && (
@@ -355,7 +219,7 @@ const Miscellaneous = ({
                   value: 'earnifi',
                   label: t('tabs.more.misc.reminderOptions.earnifi', { ns: 'profile' }),
                   onClick: () => {
-                    setHasEarnifiDialog(true)
+                    setShowEarnifiDialog(true)
                   },
                 },
                 ...calendarOptions.map((option) => ({
