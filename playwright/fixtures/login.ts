64146bd9bfe3eaf7a8da01e9bf5c024d34a4fdaa
storywithoutend <storywithoutend@gmail.com>
2023-07-25 16:50:24 +0800

    increase wait time and shard playwright tests

--- playwright/fixtures/login.ts
+++ playwright/fixtures/login.ts
@@ -33,7 +33,7 @@ export class Login {
      * A bug in rainbow kit needs time to load the provider. Could be fixed in version 1.0.4
      * Also throws eror with walletconnect if not enough time has passed.
      */
-    await this.page.waitForTimeout(10000)
+    await this.page.waitForTimeout(15000)
     this.page
       .getByRole('button', { name: 'Close' })
       .click({ timeout: 15000 })
