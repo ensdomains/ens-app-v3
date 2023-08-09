c8bc980d5b754885bc92b0434e4c946d7e6334d0
Luc <luc@lucemans.nl>
2023-08-09 13:55:38 +0000

    Bump to new username

--- e2e/specs/stateful/profile.spec.ts
+++ e2e/specs/stateful/profile.spec.ts
@@ -29,7 +29,7 @@ const profiles = [
       {
         type: 'account',
         key: 'com.discord',
-        value: 'lucemans',
+        value: 'futurealisha',
       },
       {
         type: 'account',
