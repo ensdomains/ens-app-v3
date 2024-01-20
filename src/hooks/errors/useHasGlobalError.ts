16571aab655bcfa1bc78e1b7e651a0cf2a77c29f
v1rtl <hi@v1rtl.site>
2024-01-20 17:43:23 +0200

    make debug work globally in the app

--- src/hooks/errors/useHasGlobalError.ts
+++ src/hooks/errors/useHasGlobalError.ts
@@ -1,15 +1,20 @@
-import { useMemo } from 'react'
+import { useMemo, useState } from 'react'
 
 import { useHasSubgraphSyncErrors } from '@app/utils/useHasSubgraphSyncErrors'
 
 export const useHasGlobalError = (includeLatency = false) => {
   const { error, slow } = useHasSubgraphSyncErrors()
+  const [ensjsDebug] = useState(() =>
+    typeof localStorage === 'undefined' ? '' : localStorage.getItem('subgraph-debug') || '',
+  )
 
   return useMemo(() => {
-    if (includeLatency) {
+    if (includeLatency || ensjsDebug === 'ENSJSSubgraphLatency') {
       return Boolean(error || slow)
     }
 
-    return Boolean(error)
-  }, [error, slow, includeLatency])
+    return (
+      Boolean(error) || ensjsDebug === 'ENSJSUnknownError' || ensjsDebug === 'ENSJSSubgraphError'
+    )
+  }, [includeLatency, ensjsDebug, error, slow])
 }
