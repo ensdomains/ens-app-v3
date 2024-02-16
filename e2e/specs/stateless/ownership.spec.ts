d5f8c105a473463ddd6d050e6646aa88f30cda83
tate <tate@ens.domains>
2024-02-16 11:19:12 +1100

    the final change...

--- e2e/specs/stateless/ownership.spec.ts
+++ e2e/specs/stateless/ownership.spec.ts
@@ -1122,12 +1122,12 @@ test.describe('Extend name', () => {
     await test.step('should show the correct price data', async () => {
       await expect(extendNamesModal.getInvoiceExtensionFee).toContainText('0.0033')
       await expect(extendNamesModal.getInvoiceTransactionFee).toContainText('0.0001')
-      await expect(extendNamesModal.getInvoiceTotal).toContainText('0.0034')
+      await expect(extendNamesModal.getInvoiceTotal).toContainText('0.0033')
       await expect(page.getByText('1 year extension')).toBeVisible()
     })
 
     await test.step('should show the cost comparison data', async () => {
-      await expect(page.getByTestId('year-marker-0')).toContainText('3% gas')
+      await expect(page.getByTestId('year-marker-0')).toContainText('2% gas')
       await expect(page.getByTestId('year-marker-1')).toContainText('1% gas')
       await expect(page.getByTestId('year-marker-2')).toContainText('1% gas')
     })
