9a7c747388e69a76d67493e082c814971959e4bd
v1rtl <hi@v1rtl.site>
2024-02-29 15:23:52 +0200

    compress all svgs

--- typings-custom/styled-components.d.ts
+++
@@ -1,9 +0,0 @@
-import { lightTheme } from '@ensdomains/thorin'
-import 'styled-components'
-
-type Theme = typeof lightTheme
-
-declare module 'styled-components' {
-  // eslint-disable-next-line @typescript-eslint/no-empty-interface
-  interface DefaultTheme extends Theme {}
-}
