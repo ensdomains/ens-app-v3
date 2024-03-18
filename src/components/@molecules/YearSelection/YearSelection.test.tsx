40ef5cc6968ec5a6527a1051dd8f7f19f9aebc97
v1rtl <hi@v1rtl.site>
2024-03-18 12:01:28 +0200

    fix unit tests

--- src/components/@molecules/YearSelection/YearSelection.test.tsx
+++ src/components/@molecules/YearSelection/YearSelection.test.tsx
@@ -2,21 +2,21 @@ import { render, screen } from '@app/test-utils'
 
 import { afterEach, describe, expect, it, vi } from 'vitest'
 
-import { dateToInput, formatExpiry } from '@app/utils/utils'
-
 import { YearSelection } from './YearSelection'
+
+const now = new Date()
 
 describe('YearSelection', () => {
   afterEach(() => {
     vi.resetAllMocks()
   })
   it('should render a plus minus counter if no name was provided', () => {
-    render(<YearSelection date={new Date()} setDate={() => {}} />)
+    render(<YearSelection minDate={now} date={now} setDate={() => {}} />)
 
     expect(screen.getByTestId('plus-minus-control-input')).toBeInTheDocument()
   })
   it('should render a plus minus counter if multiple names were provided (bulk extension)', () => {
-    render(<YearSelection date={new Date()} setDate={() => {}} />)
+    render(<YearSelection minDate={now} date={now} setDate={() => {}} />)
 
     expect(screen.getByTestId('plus-minus-control-input')).toBeInTheDocument()
   })
