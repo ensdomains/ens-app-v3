e8aef29591b50cd031f6fb5ccdd07a4dea5cd98f
v1rtl <hi@v1rtl.site>
2024-03-25 03:06:03 +0200

    fix unit tests

--- src/components/@atoms/Calendar/Calendar.test.tsx
+++ src/components/@atoms/Calendar/Calendar.test.tsx
@@ -2,22 +2,22 @@ import { render, screen } from '@app/test-utils'
 
 import { afterEach, describe, expect, it, vi } from 'vitest'
 
-import { dateToInput, formatExpiry } from '@app/utils/utils'
+import { formatExpiry, secondsToDate, secondsToDateInput } from '@app/utils/utils'
 
 import { Calendar } from './Calendar'
 
-const now = new Date()
-const min = new Date(2049)
+const now = 0
+const min = 0
 
 describe('Calendar', () => {
   it('should render correctly and set default attributes', () => {
-    render(<Calendar value={now} onChange={() =>{}} />)
-    expect(screen.getByTestId('calendar')).toHaveAttribute('value', dateToInput(now))
-    expect(screen.getByTestId('calendar-date')).toHaveTextContent(formatExpiry(now))
-    expect(screen.getByTestId('calendar')).toHaveAttribute('min', dateToInput(now))
+    render(<Calendar value={now} onChange={() => {}} />)
+    expect(screen.getByTestId('calendar')).toHaveAttribute('value', secondsToDateInput(now))
+    expect(screen.getByTestId('calendar-date')).toHaveTextContent(formatExpiry(secondsToDate(now)))
+    expect(screen.getByTestId('calendar')).toHaveAttribute('min', secondsToDateInput(now))
   })
   it('should allow setting minimum date', () => {
-    render(<Calendar value={now} onChange={() =>{}} min={min} name="test.eth" />)
-    expect(screen.getByTestId('calendar')).toHaveAttribute('min', dateToInput(min))
+    render(<Calendar value={now} onChange={() => {}} min={min} name="test.eth" />)
+    expect(screen.getByTestId('calendar')).toHaveAttribute('min', secondsToDateInput(min))
   })
 })
--- src/components/@molecules/DateSelection/DateSelection.test.tsx
+++ src/components/@molecules/DateSelection/DateSelection.test.tsx
@@ -4,19 +4,12 @@ import { afterEach, describe, expect, it, vi } from 'vitest'
 
 import { DateSelection } from './DateSelection'
 
-const now = new Date()
-
 describe('DateSelection', () => {
   afterEach(() => {
     vi.resetAllMocks()
   })
   it('should render a plus minus counter if no name was provided', () => {
-    render(<DateSelection minDate={now} date={now} setDate={() => {}} />)
-
-    expect(screen.getByTestId('plus-minus-control-input')).toBeInTheDocument()
-  })
-  it('should render a plus minus counter if multiple names were provided (bulk extension)', () => {
-    render(<DateSelection minDate={now} date={now} setDate={() => {}} />)
+    render(<DateSelection minDuration={0} seconds={0} setSeconds={() => {}} />)
 
     expect(screen.getByTestId('plus-minus-control-input')).toBeInTheDocument()
   })
