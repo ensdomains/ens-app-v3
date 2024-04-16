c75313287e5b221bf12a61b3fccbee53a7dbb6b3
aslobodnik <aslobodnik@gmail.com>
2024-04-16 15:38:29 -0400

    add arb1, base, and op to supported addresses

--- src/constants/supportedAddresses.ts
+++ src/constants/supportedAddresses.ts
@@ -1,1 +1,12 @@
-export const supportedAddresses = ['eth', 'btc', 'bnb', 'doge', 'ltc', 'dot', 'sol'] as const
+export const supportedAddresses = [
+  'eth',
+  'btc',
+  'bnb',
+  'doge',
+  'ltc',
+  'dot',
+  'sol',
+  'arb1',
+  'base',
+  'op',
+] as const
