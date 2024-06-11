12e5c35b2f4ea6c0500684c6813b03aee314f5db
v1rtl <hi@v1rtl.site>
2024-06-11 18:31:14 +0300

    append instead of setting

--- functions/_middleware.ts
+++ functions/_middleware.ts
@@ -65,7 +65,7 @@ const firefoxRewrite: PagesFunction = async ({ request, next }) => {
         .transform(await next())
     }
     // double-set CSP
-    request.headers.set(
+    request.headers.append(
       'Content-Security-Policy',
       "worker-src 'self' ; script-src 'self' 'sha256-UyYcl+sKCF/ROFZPHBlozJrndwfNiC5KT5ZZfup/pPc=' https://*.googletagmanager.com plausible.io static.cloudflareinsights.com *.ens-app-v3.pages.dev https://app.intercom.io https://widget.intercom.io https://js.intercomcdn.com 'wasm-unsafe-eval'; frame-ancestors 'self' https://app.safe.global;",
     )
