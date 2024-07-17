de95d39664f134cedd228d0405501a2e437b225e
Nho Huynh <nhohb@devblock.net>
2024-07-17 18:52:40 +0700

    Upgrade favicon to SVG for better rendering and update manifest.json

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -87,24 +87,15 @@ export default class MyDocument extends Document {
               <link rel="stylesheet" href="./fonts/fonts.css" />
             </>
           )}
-          <link rel="apple-touch-icon" sizes="256x256" href={makeIPFSURL('/icon/AppIcon.png')} />
-          <link
-            rel="icon"
-            type="image/png"
-            sizes="32x32"
-            href={makeIPFSURL('/favicon-32x32.png')}
-          />
-          <link
-            rel="icon"
-            type="image/png"
-            sizes="16x16"
-            href={makeIPFSURL('/favicon-16x16.png')}
-          />
+
           <link rel="manifest" href={makeIPFSURL('/manifest.webmanifest')} />
           <link rel="manifest" href={makeIPFSURL('/manifest.json')} />
-          <link rel="mask-icon" href={makeIPFSURL('/safari-pinned-tab.svg')} color="#5bbad5" />
           <meta name="msapplication-TileColor" content="#da532c" />
           <meta name="theme-color" content="#F7F7F7" />
+
+          <link rel="icon" href={makeIPFSURL('/favicon.svg')} />
+          <link rel="mask-icon" href={makeIPFSURL('/mask-icon.svg')} color="#5bbad5" />
+          <link rel="apple-touch-icon" href={makeIPFSURL('/apple-touch-icon.png')} />
 
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
