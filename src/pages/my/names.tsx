7f273f81156a6342c9ed53a4c5435282136aaff7
v1rtl <hi@v1rtl.site>
2024-07-18 23:01:11 +0300

    add warning to names list

--- src/pages/my/names.tsx
+++ src/pages/my/names.tsx
@@ -1,12 +1,24 @@
 import { useRouter } from 'next/router'
 import { ReactElement } from 'react'
 import { useTranslation } from 'react-i18next'
+import styled, { css } from 'styled-components'
 import { useAccount } from 'wagmi'
+
+import { Banner, QuestionCircleSVG } from '@ensdomains/thorin'
 
 import { NameListView } from '@app/components/@molecules/NameListView/NameListView'
 import { useProtectedRoute } from '@app/hooks/useProtectedRoute'
 import { Content } from '@app/layouts/Content'
 import { ContentGrid } from '@app/layouts/ContentGrid'
+
+const ContentContainer = styled.div(
+  ({ theme }) => css`
+    display: flex;
+    flex-direction: column;
+    gap: ${theme.space[6]};
+    color: ${theme.colors.indigoPrimary};
+  `,
+)
 
 export default function Page() {
   const { t } = useTranslation('names')
@@ -20,7 +32,14 @@ export default function Page() {
   return (
     <Content title={t('title')} singleColumnContent loading={isLoading}>
       {{
-        trailing: <NameListView address={address} selfAddress={address} />,
+        trailing: (
+          <ContentContainer>
+            <NameListView address={address} selfAddress={address} />
+            <Banner icon={<QuestionCircleSVG />} title={t('offchainWarning.title')}>
+              {t('offchainWarning.text')}
+            </Banner>
+          </ContentContainer>
+        ),
       }}
     </Content>
   )
