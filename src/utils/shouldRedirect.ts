f4ea418494ca5cf3aa27d2dec294dcbe94998993
Leon Talbert <leon.talbert@gmail.com>
2024-08-14 16:24:14 +0800

    DotBox

--- src/utils/shouldRedirect.ts
+++ src/utils/shouldRedirect.ts
@@ -2,10 +2,12 @@
 import { match, P } from 'ts-pattern'
 
 import { getSupportedChainById } from '@app/constants/chains'
+import { useDotBoxAvailabilityOffchain } from '@app/hooks/dotbox/useDotBoxAvailabilityOffchain'
 import { useRouterWithHistory } from '@app/hooks/useRouterWithHistory'
 
 type RouteParams = {
   '/unsupportedNetwork': { isConnected: boolean; chainId: number | undefined }
+  '/profile/<name>': ReturnType<typeof useDotBoxAvailabilityOffchain>
 }
 
 export const shouldRedirect = (
@@ -13,16 +15,22 @@ export const shouldRedirect = (
   destination: keyof RouteParams,
   params: RouteParams[keyof RouteParams],
 ) => {
-  // console.log('should redreict')
   match([destination, params])
     .with(
       ['/unsupportedNetwork', { isConnected: true, chainId: P.number }],
-      () => !getSupportedChainById(params.chainId) && router.pathname !== '/unsupportedNetwork',
-      () => {
-        router.push('/unsupportedNetwork')
-      },
+      (_params) =>
+        !getSupportedChainById(_params[1].chainId) && router.pathname !== '/unsupportedNetwork',
+      () => router.push('/unsupportedNetwork'),
+    )
+    .with(
+      ['/profile/<name>', { isLoading: false }],
+      (_params) =>
+        _params[1].data?.data.status !== 'AVAILABLE' &&
+        _params[1].data?.data.status !== 'UNAVAILABLE' &&
+        router.isReady,
+      () => router.push(router.query.name as string),
     )
     .otherwise(() => {
-      //   console.warn('shouldRedirect otherwise called: ', destination, params)
+      // Do nothing
     })
 }
