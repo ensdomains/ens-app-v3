9d3a01dead74547a338da648c573ee2f9eabe65b
v1rtl <hi@v1rtl.site>
2024-08-24 03:31:02 +0300

    fmt

--- src/components/pages/profile/[name]/tabs/MoreTab/NameWrapper.test.tsx
+++ src/components/pages/profile/[name]/tabs/MoreTab/NameWrapper.test.tsx
@@ -1,8 +1,9 @@
 import { render, screen } from '@app/test-utils'
+
 import { makeMockUseWrapperDataData } from '@root/test/mock/makeMockUseWrapperDataData.ts'
 import { describe, expect, it, vi } from 'vitest'
-import { NameWrapper } from './NameWrapper'
 
+import { NameWrapper } from './NameWrapper'
 
 vi.mock('./Token/WrapButton', () => ({ default: () => <div data-testid="wrap-button" /> }))
 vi.mock('./Token/UnwrapButton', () => ({ default: () => <div data-testid="unwrap-button" /> }))
@@ -11,7 +12,9 @@ describe('NameWrapper', () => {
   it('should show wrapped status for unwrapped name', () => {
     const name = 'nick.eth'
     render(
-      <NameWrapper {...({ name, isWrapped: false, wrapperData: makeMockUseWrapperDataData() } as any)} />,
+      <NameWrapper
+        {...({ name, isWrapped: false, wrapperData: makeMockUseWrapperDataData() } as any)}
+      />,
     )
     expect(screen.getByTestId('namewrapper-status')).toHaveTextContent(
       'tabs.more.token.status.unwrapped',
@@ -22,8 +25,8 @@ describe('NameWrapper', () => {
     render(
       <NameWrapper
         {...({ name, isWrapped: true, wrapperData: makeMockUseWrapperDataData('wrapped') } as any)}
-         ownerData={{
-          owner:'0xaaa'
+        ownerData={{
+          owner: '0xaaa',
         }}
       />,
     )
@@ -33,22 +36,27 @@ describe('NameWrapper', () => {
   })
   it('should show wrap button if unwrapped', () => {
     const name = 'nick.eth'
-    render(<NameWrapper address="0xaaa" {...({ name, isWrapped: false } as any)}  ownerData={{
-      owner:'0xaaa'
-    }} />)
+    render(
+      <NameWrapper
+        address="0xaaa"
+        {...({ name, isWrapped: false } as any)}
+        ownerData={{
+          owner: '0xaaa',
+        }}
+      />,
+    )
     expect(screen.getByTestId('wrap-button')).toBeVisible()
   })
   it('should show unwrap button if wrapped', () => {
     const name = 'nick.eth'
     render(
       <NameWrapper
-      isWrapped
-      
-      address={'0xaaa'}
+        isWrapped
+        address={'0xaaa'}
         {...({ name, isWrapped: true, wrapperData: makeMockUseWrapperDataData('wrapped') } as any)}
         ownerData={{
-          owner:'0xaaa',
-          ownershipLevel:'nameWrapper'
+          owner: '0xaaa',
+          ownershipLevel: 'nameWrapper',
         }}
       />,
     )
