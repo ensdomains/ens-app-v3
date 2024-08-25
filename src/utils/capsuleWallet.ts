95a1785ebe05fb7734015f8f07314e17ad5e8fbd
Leon Talbert <leon.talbert@gmail.com>
2024-08-25 19:55:56 +0800

    onramp

--- src/utils/capsuleWallet.ts
+++ src/utils/capsuleWallet.ts
@@ -8,8 +8,8 @@ import {
 } from '@usecapsule/rainbowkit-wallet'
 
 const APP_NAME = 'ENS'
-const CAPSULE_ENV: Environment = Environment.DEV
-const CAPSULE_API_KEY = ''
+const CAPSULE_ENV: Environment = Environment.BETA
+const CAPSULE_API_KEY = 'ea5557d8aed418b3c195f28c5a8dd3bb'
 
 const capsuleConstructorOpts: ConstructorOpts = {
   // Passkey Portal Branding
@@ -46,6 +46,23 @@ const capsuleWalletItegratedOpts: GetCapsuleIntegratedOpts = {
   nameOverride: `Sign in with ${APP_NAME}`,
   iconOverride: 'https://cryptologos.cc/logos/ethereum-name-service-ens-logo.png',
   iconBackgroundOverride: '#ffffff',
+  onRampConfig: {
+    network: 'ETHEREUM',
+    // Supported values: 'BASE', 'OPTIMISM', 'ARBITRUM', 'POLYGON'
+    asset: 'ETH',
+    // Supported values: 'ETHEREUM', 'USDC', 'POLYGON', 'MATIC'
+    providers: [
+      { id: 'STRIPE' },
+      // Enables Stripe
+      {
+        id: 'RAMP',
+        hostApiKey: 'your-ramp-api-key',
+      },
+      // Enables Ramp
+    ],
+    testMode: false,
+    // Enable test mode by passing 'true'
+  },
 }
 
 export const capsuleWallet = getCapsuleWalletIntegrated(capsuleWalletItegratedOpts)
