fd9bcb2df2e3a60e06c4804196bda69f84413cdb
Leon Talbert <leon.talbert@gmail.com>
2024-09-04 16:18:41 +0800

    Add linea to supported addresses

--- src/constants/supportedAddresses.ts
+++ src/constants/supportedAddresses.ts
@@ -1,3 +1,12 @@
-export const supportedAddresses = ['eth', 'btc', 'sol', 'op', 'arb1', 'base', 'matic'] as const
+export const supportedAddresses = [
+  'eth',
+  'btc',
+  'sol',
+  'op',
+  'arb1',
+  'base',
+  'matic',
+  'linea',
+] as const
 
 export type SupportedAddress = (typeof supportedAddresses)[number]
