86885ee78658cf9f40a84fdd7345f8e5c4fb41b0
sugh01 <19183308+sugh01@users.noreply.github.com>
2024-09-11 23:49:48 +0200

    FET-1618 component test

---
+++ src/components/@atoms/ErrorScreen.test.tsx
@@ -0,0 +1,24 @@
+import { render, screen } from '@app/test-utils'
+
+import { describe, expect, it, vi } from 'vitest'
+
+import ErrorScreen from './ErrorScreen'
+
+vi.mock('next/link', () => ({
+  __esModule: true,
+  default: ({ children, ...props }: { children: React.ReactNode }) => <a {...props}>{children}</a>,
+}))
+
+describe('ErrorScreen', () => {
+  it('renders not-found error correctly', () => {
+    render(<ErrorScreen errorType="not-found" />)
+    expect(screen.findAllByText('Not found')).toBeTruthy()
+    expect(screen.getByTestId('question-circle-svg')).toBeVisible()
+  })
+
+  it('renders application-error correctly', () => {
+    render(<ErrorScreen errorType="application-error" />)
+    expect(screen.findAllByText('Application error')).toBeTruthy()
+    expect(screen.getByTestId('alert-svg')).toBeVisible()
+  })
+})
