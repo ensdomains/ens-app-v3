8cf24078709b71d0cc1da9236153395bc7e28e91
Leon Talbert <leon.talbert@gmail.com>
2024-10-16 09:42:29 +0800

    Remove Thorin dep

--- src/components/@molecules/NameExpiryDesyncBanner.tsx
+++ src/components/@molecules/NameExpiryDesyncBanner.tsx
@@ -1,10 +1,51 @@
 import { useTranslation } from 'react-i18next'
+import styled, { css } from 'styled-components'
 import { useAccount } from 'wagmi'
 
-import { Banner, Button } from '@ensdomains/thorin'
+import { Banner, Button, mq, Typography } from '@ensdomains/thorin'
 
 import { createTransactionItem } from '@app/transaction-flow/transaction'
 import { useTransactionFlow } from '@app/transaction-flow/TransactionFlowProvider'
+
+const StyledButton = styled(Button)(
+  ({ theme }) => css`
+    width: 100%;
+
+    ${mq.md.min(css`
+      width: auto;
+      margin-top: -25px;
+    `)}
+  `,
+)
+
+const Container = styled.div(
+  ({ theme }) => css`
+    display: flex;
+    flex-direction: column;
+    gap: ${theme.space['4']};
+
+    ${mq.md.min(css`
+      flex-direction: row;
+      align-items: center;
+      justify-content: space-between;
+    `)}
+  `,
+)
+
+const handleSyncClick =
+  (
+    resumable: boolean,
+    resumeTransactionFlow: ReturnType<typeof useTransactionFlow>['resumeTransactionFlow'],
+    createTransactionFlow: ReturnType<typeof useTransactionFlow>['createTransactionFlow'],
+    key: string,
+    normalisedName: string,
+  ) =>
+  () => {
+    if (resumable) resumeTransactionFlow(key)
+    return createTransactionFlow(key, {
+      transactions: [createTransactionItem('syncWrappedExpiry', { name: normalisedName })],
+    })
+  }
 
 export const NameExpiryDesyncBanner = ({ normalisedName }: { normalisedName: string }) => {
   const { t } = useTranslation('profile', { keyPrefix: 'banner.expiryDesync' })
@@ -15,26 +56,24 @@ export const NameExpiryDesyncBanner = ({ normalisedName }: { normalisedName: str
   const key = `wrapExpirySync-${normalisedName}`
   const resumable = getResumable(key)
 
-  const handleSyncClick = () => {
-    if (resumable) resumeTransactionFlow(key)
-    return createTransactionFlow(key, {
-      transactions: [createTransactionItem('syncWrappedExpiry', { name: normalisedName })],
-    })
-  }
+  const _handleSyncClick = handleSyncClick(
+    resumable,
+    resumeTransactionFlow,
+    createTransactionFlow,
+    key,
+    normalisedName,
+  )
 
   return (
-    <Banner
-      alert="error"
-      title={t('title')}
-      actionButton={
-        isConnected ? (
-          <Button colorStyle="redPrimary" onClick={handleSyncClick}>
+    <Banner alert="error" title={t('title')}>
+      <Container>
+        <Typography>{t('description')}</Typography>
+        {isConnected ? (
+          <StyledButton colorStyle="redPrimary" onClick={_handleSyncClick}>
             {t('actionLabel')}
-          </Button>
-        ) : undefined
-      }
-    >
-      {t('description')}
+          </StyledButton>
+        ) : null}
+      </Container>
     </Banner>
   )
 }
