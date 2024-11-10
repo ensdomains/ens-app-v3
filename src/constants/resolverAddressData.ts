634cd294efcd4688a058a71155a33176a4e012d0
storywithoutend <storywithoutend@gmail.com>
2024-11-11 00:39:45 +0700

    Update resolverAddressData.ts

--- src/constants/resolverAddressData.ts
+++ src/constants/resolverAddressData.ts
@@ -302,7 +302,7 @@ export const KNOWN_RESOLVER_DATA: KnownResolverData = {
   ],
   '11155111': [
     {
-      address: '0x8FADE66B79cC9f707aB26799354482EB93a5B7dD',
+      address: '0x8948458626811dd0c23EB25Cc74291247077cC51',
       deployer: 'ENS Labs',
       tag: 'latest',
       isNameWrapperAware: true,
