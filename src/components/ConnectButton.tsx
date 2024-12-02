e3d028ed0f55cca987d90fdccddb079254a96d4c
Stanislav Lysak <lysaksa@amofintech.com>
2024-12-02 13:02:56 +0200

    .

--- src/components/ConnectButton.tsx
+++ src/components/ConnectButton.tsx
@@ -110,7 +110,7 @@ export const ConnectButton = ({ isTabBar, large, inHeader }: Props) => {
   return (
     <StyledButtonWrapper $large={large} $isTabBar={isTabBar}>
       <Button
-        data-testid={getTestId(isTabBar, !inHeader)}
+        data-testid={getTestId(isTabBar, inHeader)}
         onClick={() => openConnectModal?.()}
         size={breakpoints.sm || large ? 'medium' : 'small'}
         width={inHeader ? '45' : undefined}
