86f7f6cbf0ea10c3c5fde8b9b99e9fa83761f686
storywithoutend <storywithoutend@gmail.com>
2025-03-05 18:39:14 +0800

    resolver conflicts and migrate ethers to viem

--- deploy/00_deploy_legacy_resolvers.ts
+++ deploy/00_deploy_legacy_resolvers.ts
@@ -1,19 +1,18 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import fs from 'fs/promises'
-import { ethers } from 'hardhat'
 import { DeployFunction } from 'hardhat-deploy/types'
 import { HardhatRuntimeEnvironment } from 'hardhat/types'
 import { resolve } from 'path'
 
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
-  const { getNamedAccounts, deployments } = hre
+  const { getNamedAccounts, deployments, viem } = hre
   const allNamedAccts = await getNamedAccounts()
-  const { owner, deployer} = allNamedAccts
+  const { deployer } = allNamedAccts
 
-  const registry = await ethers.getContract('ENSRegistry', owner)
-  const nameWrapper = await ethers.getContract('NameWrapper', owner)
-  const ethController = await ethers.getContract('ETHRegistrarController', owner)
-  const reverseRegistrar = await ethers.getContract('ReverseRegistrar', owner)
+  const registry = await viem.getContract('ENSRegistry')
+  const nameWrapper = await viem.getContract('NameWrapper')
+  const ethController = await viem.getContract('ETHRegistrarController')
+  const reverseRegistrar = await viem.getContract('ReverseRegistrar')
 
   await deployments.deploy('OutdatedResolver', {
     from: deployer,
@@ -55,46 +54,7 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
     args: [registry.address, nameWrapper.address, ethController.address, reverseRegistrar.address],
   })
 
-  // console.log('Deploying CustomResolver...')
-  // console.log('>>', registry.address, nameWrapper.address, ethController.address, reverseRegistrar.address)
-  // await deployments.deploy('CustomResolver', {
-  //   from: allNamedAccts.deployer,
-  //   contract: JSON.parse(
-  //     await fs.readFile(resolve(__dirname, './.contracts/CustomResolver1.json'), {
-  //       encoding: 'utf8',
-  //     }),
-  //   ),
-  //   args: [registry.address, nameWrapper.address, ethController.address, reverseRegistrar.address],
-  //   gas: 5000000,
-  // })
-
   console.log('Finished deploying legacy resolvers')
-  // const resolver = await ethers.getContract('NoMulticallResolver')
-
-  // for (const { namedOwner, name, addr } of names) {
-  //   const owner = allNamedAccts[namedOwner]
-  //   const _resolver = resolver.connect(await ethers.getSigner(owner))
-  //   const _registry = registry.connect(await ethers.getSigner(owner))
-
-  //   const tx = await _registry.setResolver(namehash(name), resolver.address)
-  //   console.log(
-  //     `Setting resolver for ${name} to ${resolver.address} (tx: ${tx.hash})...`,
-  //   )
-  //   await tx.wait()
-
-  //   for (const { key, value } of addr) {
-  //     const tx2 = await _resolver['setAddr(bytes32,uint256,bytes)'](
-  //       namehash(name),
-  //       key,
-  //       value,
-  //       {
-  //         gasLimit: 100000,
-  //       },
-  //     )
-  //     console.log(`Setting address for ${key} to ${value} (tx: ${tx.hash})...`)
-  //     await tx2.wait()
-  //   }
-  // }
 
   return true
 }
