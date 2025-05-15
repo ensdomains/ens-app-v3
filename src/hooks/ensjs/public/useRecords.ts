1478fe460f62574d83c8b6e054b4b9a7d57eb17f
shrugs <mattgcondon@gmail.com>
2025-05-15 14:00:52 -0500

    add log to getRecords

--- src/hooks/ensjs/public/useRecords.ts
+++ src/hooks/ensjs/public/useRecords.ts
@@ -86,16 +86,21 @@ export const getRecordsQueryFn =
   > => {
     if (!name) throw new Error('name is required')
 
-    console.log('getRecordsQueryFn called', name)
-    const client = config.getClient({ chainId })
-    const res = await getRecords(client, {
-      // @ts-ignore
-      name,
-      ...params,
-    })
-    console.log('getRecordsQueryFn response', res)
-    if (!res) return null
-    return res as GetRecordsReturnType<TTexts, TCoins, TContentHash, TAbi>
+    try {
+      console.log('getRecordsQueryFn called', name)
+      const client = config.getClient({ chainId })
+      const res = await getRecords(client, {
+        // @ts-ignore
+        name,
+        ...params,
+      })
+      console.log('getRecordsQueryFn response', res)
+      if (!res) return null
+      return res as GetRecordsReturnType<TTexts, TCoins, TContentHash, TAbi>
+    } catch (error) {
+      console.error('getRecordsQueryFn', error)
+      throw error
+    }
   }
 
 export const matchGetRecordsQueryKeyWithInternalParams =
