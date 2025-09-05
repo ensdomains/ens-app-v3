c0620c91f5414bbf97f27fc1dbb26e72fc81f38d
sugh01 <19183308+sugh01@users.noreply.github.com>
2025-09-05 12:23:46 +0200

    e2e: stateful mainnet test fixes

--- e2e/specs/stateful/settings-primary-name.spec.ts
+++ e2e/specs/stateful/settings-primary-name.spec.ts
@@ -24,11 +24,16 @@ test.describe('Settings Tab Primary Names', () => {
     await expect(page.getByText('Settings')).toBeVisible()
 
     // Check network specific primary names are showing
-    await expect(page.getByText('Network-specific primary names')).toBeVisible()
+    await expect(page.getByText('Network-specific primary names').first()).toBeVisible()
 
     // Check all names are visible
-    for (const name of ensNames) {
-      expect(page.getByText(name)).toBeVisible()
-    }
+    // for (const name of ensNames) {
+    //   expect(page.getByText(name).first()).toBeVisible()
+    // }
+    await expect(page.getByText(ensNames[0]).first()).toBeVisible()
+    await expect(page.getByText(ensNames[1]).first()).toBeVisible()
+    await expect(page.getByText(ensNames[2]).first()).toBeVisible()
+    await expect(page.getByText(ensNames[3]).first()).toBeVisible()
+    await expect(page.getByText(ensNames[4]).first()).toBeVisible()
   })
 })
